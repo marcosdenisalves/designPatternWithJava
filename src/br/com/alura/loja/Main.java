6c6e89d8362084f0b83851b611a95212f39549c5
Denis Alves <dee.alves2@gmail.com>
2021-08-16 21:31:05 -0300

    Applying Observer pattern

--- src/br/com/alura/loja/Main.java
+++ src/br/com/alura/loja/Main.java
@@ -1,14 +1,17 @@
 package br.com.alura.loja;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import br.com.alura.loja.pedido.GerarPedido;
 import br.com.alura.loja.pedido.GerarPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBanco;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GerarPedidoHandler handler = new GerarPedidoHandler(/*dependencias*/);
+		GerarPedidoHandler handler = new GerarPedidoHandler(Arrays.asList(new SalvarPedidoNoBanco(), new EnviarEmailPedido()));
 		handler.execute(new GerarPedido("Carlos", new BigDecimal("200"),5));
 	}
 }
--- src/br/com/alura/loja/pedido/GerarPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GerarPedidoHandler.java
@@ -1,13 +1,23 @@
 package br.com.alura.loja.pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
 
 public class GerarPedidoHandler {
+
+	private List<AcaoAposGerarPedido> acoes;
+	
+	public GerarPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+		this.acoes = acoes;
+	}
 
 	public void execute(GerarPedido dados) {
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
+		
+		acoes.forEach(x -> x.executarAcao(pedido));
 	}
 }
